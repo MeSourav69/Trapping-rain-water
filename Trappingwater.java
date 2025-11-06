87917abc8e11bd38b033740157b0da3c442e1c86
SOURAV RAY <souravray288@gmail.com>
2025-11-07 00:40:13 +0530

    Add files via upload

---
+++ Trappingwater.java
@@ -0,0 +1,32 @@
+import java.util.*;
+public class Trappingwater {
+    public static int Trapewater(int numbers[]){
+        int n=numbers.length;
+        int leftmax[]=new int[n];
+        leftmax[0]=numbers[0];
+        for(int i=1; i<n; i++){
+            leftmax[i]=Math.max(numbers[i], leftmax[i-1]);
+        }
+        int rightmax[]=new int[n];
+        rightmax[n-1]=numbers[n-1];
+        for(int i=n-2; i>=0; i--){
+            rightmax[i]=Math.max(numbers[i], rightmax[i+1]);
+        }
+        int trape=0;
+        for(int i=0; i<n; i++){
+             int waterlevel=Math.min(leftmax[i],rightmax[i]);
+            trape+=waterlevel-numbers[i];
+        }
+        return trape;
+    }
+    
+    
+    
+    
+    
+    public static void main(String[] args) {
+       int numbers[]={7,2,4,0,3,2};
+       System.out.print(Trapewater(numbers));
+    }
+    
+}
